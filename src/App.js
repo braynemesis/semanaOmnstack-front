8d55ddd4b071e1f8947ed82ff624fd9a39d6cacd
Brayner <brayner.leite@cubo333.com.br>
2020-02-21 19:21:27 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuario do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/24900774?s=460&v=4"
+                alt="brayner"
+              />
+              <div className="user-info">
+                <strong>Brayner Felipe</strong>
+                <span>ReactJS, IonicJS</span>
+              </div>
+            </header>
+
+            <p>qsadasdasdasdasdasdsa. adasdasdsad</p>
+            <a href="https://braynerfelipe.com">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/24900774?s=460&v=4"
+                alt="brayner"
+              />
+              <div className="user-info">
+                <strong>Brayner Felipe</strong>
+                <span>ReactJS, IonicJS</span>
+              </div>
+            </header>
+
+            <p>qsadasdasdasdasdasdsa. adasdasdsad</p>
+            <a href="https://braynerfelipe.com">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/24900774?s=460&v=4"
+                alt="brayner"
+              />
+              <div className="user-info">
+                <strong>Brayner Felipe</strong>
+                <span>ReactJS, IonicJS</span>
+              </div>
+            </header>
+
+            <p>qsadasdasdasdasdasdsa. adasdasdsad</p>
+            <a href="https://braynerfelipe.com">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/24900774?s=460&v=4"
+                alt="brayner"
+              />
+              <div className="user-info">
+                <strong>Brayner Felipe</strong>
+                <span>ReactJS, IonicJS</span>
+              </div>
+            </header>
+
+            <p>qsadasdasdasdasdasdsa. adasdasdsad</p>
+            <a href="https://braynerfelipe.com">Acessar perfil do Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
